4f4c6cd4097603e783d9be683b240a70466f0a35
KyubinHwang <choco1drink@gmail.com>
2022-09-20 03:13:16 +0900

    setupProxy 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,10 @@ module.exports = function (app) {
   app.use(
     '/',
     createProxyMiddleware({
-      target: 'http://43.201.34.118:3306/',
-      changeOrigin: true
+      target: 'http://43.201.34.118:3306',
+      pathRewrite: {
+        '^/': '',
+      },
     })
   );
 };
