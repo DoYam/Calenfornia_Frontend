e7fb45d7fabd7b884ba3080a93d5ec5607de115e
KyubinHwang <choco1drink@gmail.com>
2022-09-30 16:59:16 +0900

    수업 필터링  수정

--- src/hooks/useSubject.jsx
+++ src/hooks/useSubject.jsx
@@ -50,7 +50,7 @@ const useSubject = () => {
     }
 
     const refreshSubject = () => {
-        axios.delete(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/`).then(()=>{}).catch()
+        axios.delete(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}`).then(()=>{}).catch()
         setUserSubject([])
         setSubCheck(false)
         setProfCheck(false)
@@ -61,8 +61,8 @@ const useSubject = () => {
 
     const deleteSubject = (e) => {
         const userSubjectid = e.target.value
-        axios.delete(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/${userSubjectid}/`).then(()=>{
-            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/`)
+        axios.delete(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/${userSubjectid}`).then(()=>{
+            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}`)
             .then((response)=> {
                 setUserSubject(response.data);
             })
@@ -78,7 +78,7 @@ const useSubject = () => {
             professor_id : professorId,
             classnum : classNumId
         }).then(()=>{
-            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/`)
+            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}`)
             .then((response)=> {
                 setUserSubject(response.data);
             }).catch()
@@ -89,7 +89,7 @@ const useSubject = () => {
         axios.get("http://43.201.34.118:3306/subject/")
         .then((response)=> {
             setAllData(response.data)
-            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}/`)
+            axios.get(`http://43.201.34.118:3306/usersubject/${localStorage.getItem('id')}`)
             .then((response)=> {
                 console.log(response.data)
                 setUserSubject(response.data)
