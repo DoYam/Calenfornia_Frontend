1eaece19c67787f852cdf025839fb97e821081e8
KyubinHwang <choco1drink@gmail.com>
2022-10-10 18:31:34 +0900

    실험

--- src/screens/Main/MainRight/InfoOnCalendar.jsx
+++ src/screens/Main/MainRight/InfoOnCalendar.jsx
@@ -10,7 +10,7 @@ const InfoOnCalendar = (props) => {
     useEffect(() => {
         axios.get("http://43.201.34.118:3306/subject/")
         .then((response)=> {
-            console.log(response.data[props.info['subject_id']]);
+            console.log(props.info['subject_id']);
             setSubjectData(response.data);
         }).catch()
     }, []);
