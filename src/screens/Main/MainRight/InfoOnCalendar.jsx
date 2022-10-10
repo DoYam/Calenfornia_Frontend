274255fdce8de5ea4f077636e2e64c3cf1af59da
KyubinHwang <choco1drink@gmail.com>
2022-10-10 18:08:11 +0900

    실험

--- src/screens/Main/MainRight/InfoOnCalendar.jsx
+++ src/screens/Main/MainRight/InfoOnCalendar.jsx
@@ -42,8 +42,7 @@ const InfoOnCalendar = (props) => {
                         </div>
                         <div>
                             <Form.Label>과목명</Form.Label>
-                            <span className='contents'>{props.info['subject_id']}</span>
-                            {/* <span className='contents'>{subList[props.info.subject_id - 1]['subject_title']}</span> */}
+                            <span className='contents'>{subList[props.info['subject_id'] - 1]['subject_title']}</span>
                         </div>
                         <div>
                             <Form.Label>교수명</Form.Label>
